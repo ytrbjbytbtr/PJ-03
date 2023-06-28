82f7bd76193c265e1c7bba8352cd4cd3f99e68af
ytrbjbytbtr <52233543+ytrbjbytbtr@users.noreply.github.com>
2023-06-28 09:39:07 +0300

    Add files via upload

---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xceed.Wpf.Toolkit;
+using System.Windows;
+using NuGet.Protocol.Core.Types;
+using Windows.UI.Xaml;
+
+namespace ConsoleApp16
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private readonly Personalizer personalizer = new Personalizer(new Repository());
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+            bool successLogin = personalizer.Login(LoginTb.Text, PasswordTb.Text);
+            MessageBox.Show(successLogin ? "Вы успешно вошли" : "Ошибка при входе");
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            bool successRegister = personalizer.Register(LoginTb.Text, PasswordTb.Text);
+            MessageBox.Show(successRegister ? "Вы успешно зарегестрировались" : "Пользовтель с таким логином уже существует");
+        }
+    }
+}
